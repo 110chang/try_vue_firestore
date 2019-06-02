c7eff113e2f119f00ad189cb19e27eb482fbd435
110chang <u@110chang.com>
2019-06-02 15:37:53 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,73 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        v-if="authorized"
+        flat
+        @click="loggingOut"
+      >
+        ログアウト
+      </v-btn>
+      <v-btn
+        v-else
+        flat
+        @click="loggingIn"
+      >
+        ログイン
+      </v-btn>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      authorized: false,
+    };
+  },
+  mounted() {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        this.$data.authorized = true;
+      } else {
+        this.$data.authorized = false;
+      }
+    });
+  },
+  methods: {
+    loggingIn() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider);
+    },
+    loggingOut() {
+      firebase.auth().signOut();
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue';
-import App from './App.vue';
+import './plugins/vuetify';
+import firebase from 'firebase/app';
 import router from './router';
 import store from './store';
+import App from './App.vue';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+console.log(process.env);
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIRESTORE_API_KEY,
+  authDomain: process.env.VUE_APP_FIRESTORE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIRESTORE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIRESTORE_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
